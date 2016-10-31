8f048052aa868bad02e77207790f203ef6a99032
mjaworski <mjaworski@opera.com>
2016-10-31 11:29:31 +0100

    ci: build pipelines improvements

--- ci/docker_build_wheels.sh
+++ ci/docker_build_wheels.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
+# note: we do not target py26 and also it is not supported by latest Cython
+rm -rf /opt/python/cp26*
+echo -e "\n\nWill build wheels for:"
+for PY in /opt/python/*/bin/python; do echo "* $(${PY} --version 2>&1)"; done
+
 for PYBIN in /opt/python/*/bin; do
-    # note: latest version of cython does not support py26 so make sure we
-    #       skip building wheels for this dist
-    if [[ ! $PYBIN == *"26"* ]]; then
-        echo -e "\n\nBuilding wheel for $PYBIN"
+    echo -e "\n\nBuilding wheel for $(${PYBIN}/python --version 2>&1)"
 
-        ${PYBIN}/pip install -r /io/doc/requirements-dev.txt
-        ${PYBIN}/pip wheel /io/ -w /io/dist/
+    ${PYBIN}/pip install -r /io/doc/requirements-dev.txt
+    ${PYBIN}/pip wheel /io/ -w /io/dist-wip/
+done
 
-    fi
+
+# Bundle external shared libraries into the wheels and fix platform tags
+echo -e "\n\nAuditing wheels:"
+for whl in dist-wip/*.whl; do
+    auditwheel repair $whl -w /io/dist/
 done
--- ci/travis_before.sh
+++ ci/travis_before.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_OS_NAME == "osx" ]]; then
 
     if [[ "$PY_VERSION" != "2.7" ]]; then
         brew update
-        brew outdated pyenv
+        brew outdated pyenv || brew upgrade pyenv
         pyenv install $PY_VERSION
         pyenv local $PY_VERSION
         eval "$(pyenv init -)"
