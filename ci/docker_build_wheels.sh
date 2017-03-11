d9ac1c0df300d734220383379c5833c4dd524647
Michał Jaworski <swistakm@gmail.com>
2017-03-11 10:07:43 +0100

    ci: travis build refinements

--- ci/docker_build_wheels.sh
+++ ci/docker_build_wheels.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -v
 
 # note: we do not target py26 and also it is not supported by latest Cython
 rm -rf /opt/python/cp26*
@@ -8,7 +9,7 @@ for PY in /opt/python/*/bin/python; do echo "* $(${PY} --version 2>&1)"; done
 for PYBIN in /opt/python/*/bin; do
     echo -e "\n\nBuilding wheel for $(${PYBIN}/python --version 2>&1)"
 
-    ${PYBIN}/pip install -r /io/doc/requirements-dev.txt
+    ${PYBIN}/pip install -r /io/doc/requirements-test.txt
     ${PYBIN}/pip wheel /io/ -w /io/dist-wip/
 done
 
