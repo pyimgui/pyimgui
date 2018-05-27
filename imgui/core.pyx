4a5d4527bf057b33c12df1a811089886708a904b
Tim Kane <tim.kane@gmail.com>
2018-05-27 21:09:13 +0100

    Added visual-doc example for get_window_draw_list Increases coverage to include add_draw_rect and get_color_u32

--- imgui/core.pyx
+++ imgui/core.pyx
@@ -1733,6 +1733,29 @@ def get_window_draw_list():
     It may be useful if you want to do your own drawing via the DrawList
     api.
 
+    .. visual-example::
+        :auto_layout:
+        :height: 100
+        :width: 200
+        :click: 10 10
+
+
+        pos_x = 10
+        pos_y = 10
+        sz = 20
+
+        draw_list = imgui.get_window_draw_list()
+
+        for i in range(0, imgui.COLOR_COUNT):
+            name = imgui.get_style_color_name(i);
+            draw_list.add_rect_filled(pos_x, pos_y, pos_x+sz, pos_y+sz, imgui.get_color_u32_idx(i));
+            imgui.dummy(sz, sz);
+            imgui.same_line();
+
+        rgba_color = imgui.get_color_u32_rgba(1, 1, 0, 1);
+        draw_list.add_rect_filled(pos_x, pos_y, pos_x+sz, pos_y+sz, rgba_color);
+
+
     Returns:
         ImDrawList*
 
