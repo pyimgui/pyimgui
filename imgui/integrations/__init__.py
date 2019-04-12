d1e1cd0f25ecf7128695b3fb529f7728a23283db
Michał Jaworski <michal.jaworski@showpad.com>
2019-04-12 15:42:27 +0200

    get rid of integrations warning

--- imgui/integrations/__init__.py
+++ imgui/integrations/__init__.py
@@ -1,20 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from warnings import warn
-
-
-warn(
-    """
-    Integration layer in imgui.integrations is still in experimental mode.
-    Expect major changes in future versions. For safety it is recommended
-    to use code vendoring for specific integration impementation.
-
-    This subpackage should become stable in 1.0.0 version.
-
-    """, FutureWarning
-)
-
 
 def compute_fb_scale(window_size, frame_buffer_size):
     win_width, win_height = window_size
