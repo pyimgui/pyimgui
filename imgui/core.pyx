260440f703acd45410f32ad68e84297606d2393f
Peter Buckner <peter.buckner@gmail.com>
2021-02-26 08:21:35 -0800

    wrap GetKeyIndex, IsKeyPressed and IsKeyDown

--- imgui/core.pyx
+++ imgui/core.pyx
@@ -6461,6 +6461,44 @@ def get_time():
     return cimgui.GetTime()
 
 
+def get_key_index(int key):
+    """Map ImGuiKey_* values into user's key index. == io.KeyMap[key]
+
+    Returns:
+       int: io.KeyMap[key]
+
+    .. wraps::
+        int GetKeyIndex(ImGuiKey imgui_key)
+    """
+    return cimgui.GetKeyIndex(key)
+
+def is_key_pressed(int key_index, bool repeat = False):
+    """Was key pressed (went from !Down to Down). 
+       If repeat=true, uses io.KeyRepeatDealy / KeyRepeatRate
+
+    Returns:
+        bool: True if specified key was pressed this frame
+
+    .. wraps::
+        bool IsKeyPressed(int user_key_index)
+    """
+    return cimgui.IsKeyPressed(key_index, repeat)
+
+def is_key_down(int key_index):
+    """Returns if key is being help -- io.KeysDown[user+key_index].
+       Note that imgui doesn't knwo th esemantic of each entry of
+       io.KeysDown[]. User your own indices/enums according to how
+       your backend/engine stored them into io.KeysDown[].
+      
+    Returns:
+       bool: True if specified key is being held.
+
+    .. wraps::
+        bool IsKeyDown(int user_key_index)
+    """
+    return cimgui.IsKeyDown(key_index)
+
+
 def is_mouse_hovering_rect(
     float r_min_x, float r_min_y,
     float r_max_x, float r_max_y,
@@ -6505,17 +6543,6 @@ def is_mouse_double_clicked(int button = 0):
     """
     return cimgui.IsMouseDoubleClicked(button)
 
-
-def get_key_index(int key):
-    return cimgui.GetKeyIndex(key)
-
-def is_key_pressed(int key_index, bool repeat = False):
-    """Returns if the key was pressed this frame"""
-    return cimgui.IsKeyPressed(key_index, repeat)
-
-def is_key_down(int key_index):
-    """Returns if the key was down this frame"""
-    return cimgui.IsKeyDown(key_index)
 
 def is_mouse_clicked(int button = 0, bool repeat = False):
     """Returns if the mouse was clicked this frame.
