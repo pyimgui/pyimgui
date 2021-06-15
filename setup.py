ebc089c999cf81e4de7e1eae0897eb08003d21c1
pvallet <luap.vallet@gmail.com>
2021-06-15 08:47:36 +0200

    Fixing the external pyimgui build

--- setup.py
+++ setup.py
@@ -87,8 +87,7 @@ def extension_sources(path):
             'imgui-cpp/imgui_draw.cpp',
             'imgui-cpp/imgui_demo.cpp',
             'imgui-cpp/imgui_widgets.cpp',
-            'imgui-cpp/imgui_tables.cpp',
-            'config-cpp/py_imconfig.cpp'
+            'imgui-cpp/imgui_tables.cpp'
         ]
 
     sources += [
