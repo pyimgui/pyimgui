5c355762761ce874c675d089deae31c14627eaf1
Samuel Ernst <samuel.ernst@epfl.ch>
2021-08-31 10:30:31 +0200

    linux build config

--- ci/docker_build_wheels.sh
+++ ci/docker_build_wheels.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#travis_fold() {
-#  local action=$1
-#  local name=$2
-#  echo -en "travis_fold:${action}:${name}\r"
-#}
+travis_fold() {
+  local action=$1
+  local name=$2
+  echo -en "travis_fold:${action}:${name}\r"
+}
 
 # note: we do not target py26 and also it is not supported by latest Cython
 rm -rf /opt/python/cp26*
@@ -13,19 +13,19 @@ echo
 
 for PYBIN in /opt/python/*/bin; do
     FOLDNAME="wheel-$(basename $(dirname ${PYBIN}))"
-    #travis_fold start ${FOLDNAME}
+    travis_fold start ${FOLDNAME}
     echo -e "Building wheel for $(${PYBIN}/python --version 2>&1)"
 
     ${PYBIN}/pip install -r /io/doc/requirements-test.txt
     ${PYBIN}/pip wheel /io/ -w /io/dist-wip/
-    #travis_fold end ${FOLDNAME}
+    travis_fold end ${FOLDNAME}
 done
 
 
-#travis_fold start auditwheels
+travis_fold start auditwheels
 # Bundle external shared libraries into the wheels and fix platform tags
 echo -e "Auditing wheels:"
 for whl in /io/dist-wip/*.whl; do
     auditwheel repair $whl -w /io/dist/
 done
-#travis_fold end auditwheels
+travis_fold end auditwheels
--- ci/travis_before.sh
+++ ci/travis_before.sh
@@ -2,14 +2,14 @@
 # stop on failures immediately
 set -e
 
-#travis_fold() {
-#  local action=$1
-#  local name=$2
-#  echo -en "travis_fold:${action}:${name}\r"
-#}
+travis_fold() {
+  local action=$1
+  local name=$2
+  echo -en "travis_fold:${action}:${name}\r"
+}
 
 if [[ $DOCKER_IMAGE ]]; then
-    #travis_fold start docker-pull
+    travis_fold start docker-pull
     docker pull $DOCKER_IMAGE
-    #travis_fold end docker-pull
+    travis_fold end docker-pull
 fi
