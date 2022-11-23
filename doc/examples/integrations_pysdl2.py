a7dc7f2eae525d9ef960182d35fa82a5f5b724bd
TJ Porter <taylerporter@gmail.com>
2022-11-22 23:52:02 -0600

    Cleaned up doc/examples Added shebang to all examples Added encoding string for all examples Standardized import order for cleaner look Changed all exits to sys.exit as exit is not guaranteed Changed all exits to use proper exit code (1 for error, 0 for no error) Ran black over examples to cleanup a lot of PEP8 errors and standardize formatting.

--- doc/examples/integrations_pysdl2.py
+++ doc/examples/integrations_pysdl2.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from sdl2 import *
-import ctypes
-import OpenGL.GL as gl
 
-import imgui
 from imgui.integrations.sdl2 import SDL2Renderer
 from testwindow import show_test_window
+from sdl2 import *
+import OpenGL.GL as gl
+import ctypes
+import imgui
+import sys
 
 
 def main():
@@ -31,26 +33,26 @@ def main():
             if imgui.begin_menu("File", True):
 
                 clicked_quit, selected_quit = imgui.menu_item(
-                    "Quit", 'Cmd+Q', False, True
+                    "Quit", "Cmd+Q", False, True
                 )
 
                 if clicked_quit:
-                    exit(1)
+                    sys.exit(0)
 
                 imgui.end_menu()
             imgui.end_main_menu_bar()
 
         show_test_window()
-        #imgui.show_test_window()
+        # imgui.show_test_window()
 
         if show_custom_window:
             is_expand, show_custom_window = imgui.begin("Custom window", True)
             if is_expand:
                 imgui.text("Bars")
-                imgui.text_colored("Eggs", 0.2, 1., 0.)
+                imgui.text_colored("Eggs", 0.2, 1.0, 0.0)
             imgui.end()
 
-        gl.glClearColor(1., 1., 1., 1)
+        gl.glClearColor(1.0, 1.0, 1.0, 1)
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 
         imgui.render()
@@ -68,8 +70,11 @@ def impl_pysdl2_init():
     window_name = "minimal ImGui/SDL2 example"
 
     if SDL_Init(SDL_INIT_EVERYTHING) < 0:
-        print("Error: SDL could not initialize! SDL Error: " + SDL_GetError().decode("utf-8"))
-        exit(1)
+        print(
+            "Error: SDL could not initialize! SDL Error: "
+            + SDL_GetError().decode("utf-8")
+        )
+        sys.exit(1)
 
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1)
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24)
@@ -85,24 +90,36 @@ def impl_pysdl2_init():
     SDL_SetHint(SDL_HINT_MAC_CTRL_CLICK_EMULATE_RIGHT_CLICK, b"1")
     SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, b"1")
 
-    window = SDL_CreateWindow(window_name.encode('utf-8'),
-                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                              width, height,
-                              SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE)
+    window = SDL_CreateWindow(
+        window_name.encode("utf-8"),
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        width,
+        height,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE,
+    )
 
     if window is None:
-        print("Error: Window could not be created! SDL Error: " + SDL_GetError().decode("utf-8"))
-        exit(1)
+        print(
+            "Error: Window could not be created! SDL Error: "
+            + SDL_GetError().decode("utf-8")
+        )
+        sys.exit(1)
 
     gl_context = SDL_GL_CreateContext(window)
     if gl_context is None:
-        print("Error: Cannot create OpenGL Context! SDL Error: " + SDL_GetError().decode("utf-8"))
-        exit(1)
+        print(
+            "Error: Cannot create OpenGL Context! SDL Error: "
+            + SDL_GetError().decode("utf-8")
+        )
+        sys.exit(1)
 
     SDL_GL_MakeCurrent(window, gl_context)
     if SDL_GL_SetSwapInterval(1) < 0:
-        print("Warning: Unable to set VSync! SDL Error: " + SDL_GetError().decode("utf-8"))
-        exit(1)
+        print(
+            "Warning: Unable to set VSync! SDL Error: " + SDL_GetError().decode("utf-8")
+        )
+        sys.exit(1)
 
     return window, gl_context
 
