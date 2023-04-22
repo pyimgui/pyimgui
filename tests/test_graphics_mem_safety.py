ea78e885f0ec9305f2694445bb4d2b788e2fe328
KinoxKlark <samuel.ernst.09@gmail.com>
2023-04-22 08:15:23 +0200

    Add some logging for test_graphics_mem_safety error on appveyor

--- tests/test_graphics_mem_safety.py
+++ tests/test_graphics_mem_safety.py
@@ -20,6 +20,7 @@ def io():
 
     return io
 
+@pytest.mark.flaky(reruns=10, reruns_delay=5)
 def test_texture_id_int_reference(context, io):
 
     # See issue #248 (https://github.com/pyimgui/pyimgui/issues/248)
@@ -40,9 +41,13 @@ def test_texture_id_int_reference(context, io):
 
         imgui.render()
         
+        print('DEBUG: texture_id: [%i]' % i, texture_id)
+        
         draw_data = imgui.get_draw_data()
+        print('DEBUG: commands_list has', len(draw_data.commands_lists), 'elem')
         for commands in draw_data.commands_lists:
             for command in commands.commands:
+                print(' - type texture id:', type(command.texture_id))
                 assert type(command.texture_id) is int
 
 def test_texture_id_keep_type(context, io):
